2de7c111e252656989728885585f987f26e66774
Victor Mendoza <mvictorm@gmail.com>
2018-08-24 16:30:50 -0400

    tutorial de rails al 24-08-2018

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,64 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except:[:show,:index]
+  before_action :set_article, except: [:index, :new, :create ]
+
+  #get
+  def index
+    @articles = Article.all    
+  end
+  
+  #get:id
+  def show
+    @article.update_visits_count
+    @comment = Comment.new
+  end 
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    
+
+  end
+
+  def create
+    @article = current_user.articles.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end  
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end  
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def validate_user
+    redirect_to new_user_session_path, notice: "Necesitas Iniciar Sesión"
+  end  
+
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
+
+
+end
+
